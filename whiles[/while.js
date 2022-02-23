7af91d159f9866cc6e04451f9dcd21359906743c
Luiz Felipe <97800827+LuizFelipeTrindade@users.noreply.github.com>
2022-02-23 16:17:48 -0300

    commit

---
+++ whiles[/while.js
@@ -0,0 +1,17 @@
+//whiles
+
+function comerPizza() {
+    while(comerPizza()){
+        console.log('Sim')
+    }
+}
+
+let idade = 65
+
+if(idade < 16) {
+    console.log('Não Vota')
+} else if (idade > 17 && idade < 65){ 
+    console.log('Voto Obrigatório')
+} if(idade >= 16 && idade <= 65) {
+    console.log('Voto opcional')
+}
